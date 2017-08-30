d285c4a041e3ba4685ca5083d7fdf3cdcfc37a87
Thomas Biesaart <thomas.biesaart@gmail.com>
2017-08-30 15:25:07 +0200

    Move git configuration to git config file

--- dashfiles-post-install.sh
+++ dashfiles-post-install.sh
@@ -5,21 +5,6 @@ if [ "$(archlinux-java get)" != "java-8-jdk" ]; then
     sudo archlinux-java set java-8-jdk
 fi
 
-# Prompt for global git configuration
-if [ -z "$(git config user.name)" ]; then
-    printf "No name was set in git. What is your name? "
-    read gitName
-    git config --global user.name "$gitName"
-fi
-
-if [ -z "$(git config user.email)" ]; then
-    printf "No email was set in git. What is your email? "
-    read gitEmail
-    git config --global user.email "$gitEmail"
-fi
-
-git config --global remote.origin.prune true
-
 # Install Vundle
 if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then
     echo "Installing Vundle..."
