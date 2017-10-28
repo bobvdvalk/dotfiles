293594d34daba7f8b3b19904decdb4a04701cd14
Bob van der Valk <info@mawoo.nl>
2017-10-28 12:54:50 +0200

    Do not remove obsolete folders

--- dashfiles-post-install.sh
+++ dashfiles-post-install.sh
@@ -23,8 +23,5 @@ gsettings set org.gnome.Terminal.Legacy.Settings default-show-menubar false
 # Enable gdm
 sudo systemctl disable lightdm.service && sudo systemctl enable gdm
 
-# Delete obsolete folders
-rm -rf ~/Music ~/Videos ~/Pictures ~/Desktop ~/Public ~/Templates ~/Videos
-
 # Enable night light
 gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
